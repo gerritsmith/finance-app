4122598458f414aa587735d7fc8dae9ea9394989
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-04-22 17:36:50 -0500

    head, footer bootstrap fragments added, index.html displayed

--- src/main/java/io/github/gerritsmith/financeapp/controller/HomeController.java
+++ src/main/java/io/github/gerritsmith/financeapp/controller/HomeController.java
@@ -2,15 +2,13 @@ package io.github.gerritsmith.financeapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String displayHomePage() {
-        return "<h1>Hello, World!</h1>";
+        return "index";
     }
 
 }
