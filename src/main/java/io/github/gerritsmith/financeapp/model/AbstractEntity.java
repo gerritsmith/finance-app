31d22d30114d84563d85ee7e521008f84b28987f
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-04-22 21:04:31 -0500

    added user entity

---
+++ src/main/java/io/github/gerritsmith/financeapp/model/AbstractEntity.java
@@ -0,0 +1,39 @@
+package io.github.gerritsmith.financeapp.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    // Getters
+    public long getId() {
+        return id;
+    }
+
+    // Equals, hash, toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id;
+    }
+
+}
