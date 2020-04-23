5b097ea58e79e03a0184860ce3d64469265ad97b
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-04-23 03:34:28 -0500

    new users can register

--- src/main/java/io/github/gerritsmith/financeapp/controller/HomeController.java
+++ src/main/java/io/github/gerritsmith/financeapp/controller/HomeController.java
@@ -1,12 +1,25 @@
 package io.github.gerritsmith.financeapp.controller;
 
 import io.github.gerritsmith.financeapp.dto.LoginFormDTO;
+import io.github.gerritsmith.financeapp.dto.RegisterFormDTO;
+import io.github.gerritsmith.financeapp.model.User;
+import io.github.gerritsmith.financeapp.service.UserExistsException;
+import io.github.gerritsmith.financeapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/")
     public String displayHomePage() {
@@ -23,6 +36,38 @@ public class HomeController {
     public String displayUserPage(Model model) {
         model.addAttribute("title", "User Home");
         return "user/home";
+    }
+
+    @GetMapping("/register")
+    public String displayRegisterUserForm(Model model) {
+        model.addAttribute("registerFormDTO", new RegisterFormDTO());
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String processRegisterUserForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
+                                          Errors errors) {
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("verifyPassword",
+                               "verifyPassword.mismatch",
+                               "passwords do not match");
+        }
+        if (errors.hasErrors()) {
+            return "user/register";
+        }
+        try {
+            String username = registerFormDTO.getUsername();
+            User newUser = new User(username, password);
+            userService.saveUser(newUser);
+        } catch (UserExistsException e) {
+            errors.rejectValue("username",
+                    "username.alreadyExists",
+                    "username is already taken");
+            return "user/register";
+        }
+        return "redirect:/login";
     }
 
 }
