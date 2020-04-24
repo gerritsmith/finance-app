1bd15616092beb026a3f0ebfe6018dbf9ce20961
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-04-23 22:23:33 -0500

    delivery form validation seems to work

--- src/main/java/io/github/gerritsmith/financeapp/controller/DeliveryController.java
+++ src/main/java/io/github/gerritsmith/financeapp/controller/DeliveryController.java
@@ -1,7 +1,14 @@
 package io.github.gerritsmith.financeapp.controller;
 
+import io.github.gerritsmith.financeapp.dto.DeliveryFormDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class DeliveryController {
@@ -9,6 +16,18 @@ public class DeliveryController {
     @GetMapping("/deliveries")
     public String displayDeliveriesHome() {
         return "delivery/home";
+    }
+
+    @GetMapping("/delivery/new")
+    public String displayNewDeliveryForm(Model model) {
+        model.addAttribute("deliveryFormDTO", new DeliveryFormDTO());
+        return "delivery/form";
+    }
+
+    @PostMapping("/delivery/new")
+    public String processNewDeliveryForm(@ModelAttribute @Valid DeliveryFormDTO deliveryFormDTO,
+                                         Errors errors) {
+        return "delivery/form";
     }
 
 }
---
+++ src/main/java/io/github/gerritsmith/financeapp/dto/DeliveryFormDTO.java
@@ -0,0 +1,47 @@
+package io.github.gerritsmith.financeapp.dto;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DeliveryFormDTO {
+
+    @NotNull(message = "date is required")
+    @PastOrPresent(message = "date can't be in the future")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @NotNull(message = "time is required")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime time;
+
+    @Pattern(regexp = "(\\d+\\.?\\d{0,2}|\\.\\d{1,2})?", message = "amount must be in the form 0000.00")
+    private String total;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+}
