817803554c4caa278e58dec0571cd704e521b8be
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-05-01 22:38:43 -0500

    additional fields displayed on delivery form

--- src/main/java/io/github/gerritsmith/financeapp/dto/DeliveryFormDTO.java
+++ src/main/java/io/github/gerritsmith/financeapp/dto/DeliveryFormDTO.java
@@ -4,6 +4,7 @@ import io.github.gerritsmith.financeapp.model.Delivery;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.*;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -17,6 +18,18 @@ public class DeliveryFormDTO {
     @NotNull(message = "time is required")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime time;
+
+    @Pattern(regexp = "(\\d+\\.?\\d{0,2}|\\.\\d{1,2})?", message = "app miles must be in the form 0.00")
+    private String appMiles;
+
+    @Pattern(regexp = "(\\d+\\.?\\d{0,2}|\\.\\d{1,2})?", message = "app wait time must be in the form 0.00")
+    private String appWaitTime;
+
+    @Pattern(regexp = "(\\d+\\.?\\d{0,2}|\\.\\d{1,2})?", message = "total miles must be in the form 0.0")
+    private String totalMiles;
+
+    @Pattern(regexp = "\\d*", message = "total time must a non-negative integer")
+    private String totalTime;
 
     @Pattern(regexp = "(\\d+\\.?\\d{0,2}|\\.\\d{1,2})?", message = "amount must be in the form 0000.00")
     private String total;
@@ -49,6 +62,38 @@ public class DeliveryFormDTO {
         this.time = time;
     }
 
+    public String getAppMiles() {
+        return appMiles;
+    }
+
+    public void setAppMiles(String appMiles) {
+        this.appMiles = appMiles;
+    }
+
+    public String getAppWaitTime() {
+        return appWaitTime;
+    }
+
+    public void setAppWaitTime(String appWaitTime) {
+        this.appWaitTime = appWaitTime;
+    }
+
+    public String getTotalMiles() {
+        return totalMiles;
+    }
+
+    public void setTotalMiles(String totalMiles) {
+        this.totalMiles = totalMiles;
+    }
+
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(String totalTime) {
+        this.totalTime = totalTime;
+    }
+
     public String getTotal() {
         return total;
     }
@@ -56,5 +101,5 @@ public class DeliveryFormDTO {
     public void setTotal(String total) {
         this.total = total;
     }
-
+    
 }
