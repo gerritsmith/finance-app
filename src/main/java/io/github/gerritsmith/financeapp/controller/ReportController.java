061442df098f94d78a0849a2beb88e191c085b07
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-05-06 20:41:11 -0500

    day report request mapped

--- src/main/java/io/github/gerritsmith/financeapp/controller/ReportController.java
+++ src/main/java/io/github/gerritsmith/financeapp/controller/ReportController.java
@@ -1,7 +1,13 @@
 package io.github.gerritsmith.financeapp.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class ReportController {
@@ -9,6 +15,14 @@ public class ReportController {
     @GetMapping("/reports")
     public String displayReportsHome() {
         return "report/home";
+    }
+
+    @GetMapping("/reports/by-day")
+    public String displayDayReport(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                               LocalDate date,
+                                   Model model) {
+        model.addAttribute("date", date);
+        return "report/day";
     }
 
 }
