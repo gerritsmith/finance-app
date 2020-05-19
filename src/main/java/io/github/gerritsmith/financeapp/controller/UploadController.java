5327be13da52a85415da304feafd2e0fed26d418
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-05-18 21:20:52 -0500

    can load shift csv rows into app memory

--- src/main/java/io/github/gerritsmith/financeapp/controller/UploadController.java
+++ src/main/java/io/github/gerritsmith/financeapp/controller/UploadController.java
@@ -1,7 +1,21 @@
 package io.github.gerritsmith.financeapp.controller;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import io.github.gerritsmith.financeapp.dto.upload.ShiftCSVRow;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
 
 @Controller
 public class UploadController {
@@ -11,6 +25,27 @@ public class UploadController {
         return "upload/home";
     }
 
-
+    @PostMapping("/upload/shifts")
+    public String uploadShiftCSVFile(@RequestParam MultipartFile file, Model model) {
+        if (file.isEmpty()) {
+            model.addAttribute("errorMessage", "File is empty!");
+            model.addAttribute("isSuccessful", false);
+        } else {
+            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+                CsvToBean<ShiftCSVRow> csvToBean = new CsvToBeanBuilder<ShiftCSVRow>(reader)
+                        .withType(ShiftCSVRow.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+                List<ShiftCSVRow> rows = csvToBean.parse();
+                // TODO: create shift entities and save to database
+                model.addAttribute("rows", rows);
+                model.addAttribute("isSuccessful", true);
+            } catch (IOException e) {
+                model.addAttribute("errorMessage", "An error occurred while processing the CSV file.");
+                model.addAttribute("isSuccessful", false);
+            }
+        }
+        return "/upload/shifts";
+    }
 
 }
---
+++ src/main/java/io/github/gerritsmith/financeapp/dto/upload/ShiftCSVRow.java
@@ -0,0 +1,93 @@
+package io.github.gerritsmith.financeapp.dto.upload;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ShiftCSVRow {
+
+    @CsvDate(value = "M/d/yy")
+    @CsvBindByName(column = "Date")
+    private LocalDate date;
+
+    @CsvDate(value = "h:mm a")
+    @CsvBindByName(column = "Start Time")
+    private LocalTime startTime;
+
+    @CsvDate(value = "h:mm a")
+    @CsvBindByName(column = "End Time")
+    private LocalTime endTime;
+
+    @CsvBindByName(column = "Start Mileage")
+    private Integer startMileage;
+
+    @CsvBindByName(column = "End Mileage")
+    private Integer endMileage;
+
+    @CsvBindByName(column = "Miles")
+    private Double miles;
+
+    @CsvBindByName(column = "MPG")
+    private Double mpg;
+
+    public ShiftCSVRow() {}
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getStartMileage() {
+        return startMileage;
+    }
+
+    public void setStartMileage(Integer startMileage) {
+        this.startMileage = startMileage;
+    }
+
+    public Integer getEndMileage() {
+        return endMileage;
+    }
+
+    public void setEndMileage(Integer endMileage) {
+        this.endMileage = endMileage;
+    }
+
+    public Double getMiles() {
+        return miles;
+    }
+
+    public void setMiles(Double miles) {
+        this.miles = miles;
+    }
+
+    public Double getMpg() {
+        return mpg;
+    }
+
+    public void setMpg(Double mpg) {
+        this.mpg = mpg;
+    }
+
+}
